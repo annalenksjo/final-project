0ed251810770c8a2ffe67bad5fe32e7477f8a320
annalenksjo <75678284+annalenksjo@users.noreply.github.com>
2021-05-31 15:48:49 +0200

    dependencies

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/challengeAppAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
