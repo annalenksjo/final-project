b643fbf43dc14a6503a1402fdc1a156e6081ef38
annalenksjo <75678284+annalenksjo@users.noreply.github.com>
2021-06-02 13:33:39 +0200

    backend again

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,61 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import listEndpoints from 'express-list-endpoints'
-import crypto from 'crypto'
 import bcrypt from 'bcrypt'
+import crypto from 'crypto'
 
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/challengeAppAPI'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+
+// next step: add Challenge model & get for challenge
+// add authentication on those endpoints 
+// em: start on FE
+
+// const Challenge = mongoose.model('Challenge', {
+//   name: {
+//     type: String
+//   },
+//   image: {
+//     type: String // url or file
+//   },
+//   description: {
+//     type: String
+//   },
+//   goal: {
+//     type: Number
+//   },
+//   prize: {
+//     type: String
+//   }
+// })
+
+// const Game = mongoose.model('Game', {
+//   challenge: {
+//     // get object from the chosen challenge
+//   },
+//   players: {
+//      user1Object: {
+//       win: {
+//         type: Boolean
+//       },
+//       score: {
+//         type: Number
+//       }
+//     },
+//      user2Object: {
+//       win: {
+//         type: Boolean
+//       },
+//       score: {
+//         type: Number
+//       }
+//     }
+//   },
+//   active: {
+//     type: Boolean,
+//   }
+// })
 
 const User = mongoose.model('User', {
   username: {
@@ -17,20 +66,19 @@ const User = mongoose.model('User', {
     unique: true,
     trim: true,
     minlenght: 3,
-    maxlenght: 30,
+    maxlenght: 30
   },
   password: {
     type: String,
     required: true,
     minlenght: 5,
-    maxlenght: 25,
+    maxlenght: 25
   },
   accessToken: {
     type: String,
-    default: () => crypto.randomBytes(128).toString('hex'),
-  },
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
 })
-
 
 const port = process.env.PORT || 8080
 const app = express()
@@ -39,59 +87,55 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
-
-//GET
+// GET
 app.get('/', (req, res) => {
   res.send(listEndpoints(app))
 })
 
 app.get('/users', async (req, res) => {
-  // needs to have a query for the user to filter on users
+  // to filter on useraccounts -> users?useraccount='value'
+  // should only send username (not accesT & pw)
   const { useraccount } = req.query
-  ///users?user='value'
 
-  let allUsers = await User.find()
+  try {
+    let allUsers = await User.find()
 
-  if (useraccount) {
-    allUsers = allUsers.filter(user => user.username.toLowerCase().includes(useraccount.toLowerCase()))
-  }    
-    res.json(allUsers)
-    
-  //   } else {
-  //     res.status(404).json({ error: 'No users found'})
-  //   } // Is this triggered if the user array is 0 after quering for users?
-    
-  // } catch (error) {
-  //   res.status(400).json({error: 'Not found'})
-  // }
+    if (useraccount) {
+      allUsers = allUsers.filter((user) => user.username.toLowerCase()
+        .includes(useraccount.toLowerCase())
+      )
+    }    
+      res.json(allUsers)
+      // if there is no user (array.lenght = 0) räcker det o visa detta i FE?
+  } catch (error) {
+      res.status(400).json({ success: false, message: 'Invalid request', error})
+  }
 })
 
+// DELETE
 
-//DELETE
+// PATCH
 
-//PATCH
-
-//POST
+// POST
 
 // USER REGISTER
 app.post('/register', async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password } = req.body
 
   try {
-    const salt = bcrypt.genSaltSync();
+    const salt = bcrypt.genSaltSync()
 
     const newUser = await new User({
       username,
-      password: bcrypt.hashSync(password, salt),
-    }).save();
+      password: bcrypt.hashSync(password, salt)
+    }).save()
 
     res.json({
       success: true,
       userID: newUser._id,
       user: newUser.username,
-      accessToken: newUser.accessToken,
-    });
+      accessToken: newUser.accessToken
+    })
   } catch (error) {
     if (error.code === 11000) {
       res.status(409).json({ success: false, message: 'Username already exists', error })
@@ -118,7 +162,7 @@ app.post('/login', async (req, res) => {
         })
     } else if (user && !bcrypt.compareSync(password, user.password)) {
       res.status(404).json({ success: false, message: 'Wrong password'})
-    }
+    } // Maybe remove this for security reasons
       else {
       res.status(404).json({ success: false, message: 'Username not found' })
     }
@@ -126,7 +170,6 @@ app.post('/login', async (req, res) => {
     res.status(400).json({ success: false, message: 'Sorry! Invalid request', error });
   }
 })
-
 
 // Start the server
 app.listen(port, () => {
